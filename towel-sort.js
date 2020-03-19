de261f276eded2a7d218534f59d38b7b54a68ec3
Igor-14-05 <60569782+Igor-14-05@users.noreply.github.com>
2020-03-19 11:55:15 +0500

    feat: add file js

---
+++ towel-sort.js
@@ -0,0 +1,26 @@
+module.exports = function towelSort (matr) {
+    var res = [];
+    if (matr==undefined)
+    { 
+      return [];
+    }
+    for (var i = 0; i < matr.length; i++)
+    {
+        var s = matr[i];
+        if (i%2==0)
+        {
+            for (var j = 0; j < s.length; j++)
+            {
+                    res.push(s[j]);
+            }
+        }
+        else 
+        {
+            for (var j = s.length-1; j >= 0; j--)
+            {
+                    res.push(s[j]);
+            }
+        }
+    }
+    return res;
+  }
